77d9470146ad6c8ed3883e8959ce4954a86bec6e
grgml <grigormalo@gmail.com>
2022-02-17 13:02:25 +0100

    create discover page

---
+++ packages/client/app/ui/discover/sidebarItems.js
@@ -0,0 +1,36 @@
+const sidebarItems = [
+  {
+    title: 'Course',
+    placeholder: 'Search for courses',
+  },
+  {
+    title: 'Question change',
+    placeholder: 'Search for Question change',
+  },
+  {
+    title: 'Vision + change',
+    placeholder: 'Search for Vision + change',
+  },
+  {
+    title: 'Unit',
+    placeholder: 'Search for unit',
+  },
+  {
+    title: 'Blooms Level',
+    placeholder: 'Search by blooms level',
+  },
+  {
+    title: 'Reading Level',
+    placeholder: 'Search by reading level',
+  },
+  {
+    title: 'Future Phisicians',
+    placeholder: 'Search for future phisicians',
+  },
+  {
+    title: 'Learning Objectives',
+    placeholder: 'Search for learning objectives',
+  },
+]
+
+export default sidebarItems
