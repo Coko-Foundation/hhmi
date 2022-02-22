0a07819336ec5c0fb8b440d488eb510f89f02c90
grgml <grigormalo@gmail.com>
2022-02-22 18:11:05 +0100

    feat(ui): create QuestionList component with associated story; create Sidebar story

--- packages/client/app/ui/dashboard/Dashboard.js
+++ packages/client/app/ui/dashboard/Dashboard.js
@@ -1,22 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { useHistory } from 'react-router-dom'
-import { grid, th } from '@coko/client'
-import { Button, Collapse, List, QuestionItem } from '../common'
-
-const LinkWithoutStyles = styled.a`
-  color: inherit;
-  text-decoration: none;
-  width: 100%;
-
-  &:hover,
-  &:focus,
-  &:active {
-    color: inherit;
-    text-decoration: none;
-  }
-`
+import { grid } from '@coko/client'
+import { Button, Collapse, QuestionList } from '../common'
 
 const Wrapper = styled.div`
   > div.ant-collapse > div.ant-collapse-item > .ant-collapse-header {
@@ -33,27 +19,17 @@ const Wrapper = styled.div`
       margin-left: auto;
     }
   }
-
-  ul > li:not(:last-child) {
-    border-bottom: 1px solid ${th('colorSecondary')};
-    padding: ${grid(3)} 0;
-  }
 `
 
 // QUESTION how to handle search, filter and pagination with multiple sections
 const Dashboard = props => {
   const {
     className,
-
     authorItems,
     editorItems,
     reviewerItems,
-    withTotalCount,
-    authorTotalCountNumber,
     onClickCreateQuestion,
   } = props
-
-  const history = useHistory()
 
   const handleClickCreate = e => {
     e.stopPropagation()
@@ -72,46 +48,28 @@ const Dashboard = props => {
           header="Author items"
           key="author"
         >
-          <List
-            dataSource={authorItems}
-            renderItem={itemProps => (
-              <List.Item>
-                <LinkWithoutStyles
-                  href={`question/${itemProps.id}`}
-                  onClick={e => {
-                    e.preventDefault()
-                    e.stopPropagation()
-                    history.push(`question/${itemProps.id}`)
-                  }}
-                >
-                  <QuestionItem {...itemProps} />
-                </LinkWithoutStyles>
-              </List.Item>
-            )}
-            showTotalCount={withTotalCount}
-            totalCount={authorTotalCountNumber}
+          <QuestionList
+            questions={authorItems}
+            showSearch={false}
+            showSort={false}
           />
         </Collapse.Panel>
 
         <Collapse.Panel header="Reviewer items" key="reviewer">
-          <List
-            dataSource={reviewerItems}
-            renderItem={itemProps => (
-              <li>
-                <QuestionItem {...itemProps} />
-              </li>
-            )}
+          <QuestionList
+            questions={reviewerItems}
+            showSearch={false}
+            showSort={false}
+            showTotalCount={false}
           />
         </Collapse.Panel>
 
         <Collapse.Panel header="Editor items" key="editor">
-          <List
-            dataSource={editorItems}
-            renderItem={itemProps => (
-              <li>
-                <QuestionItem {...itemProps} />
-              </li>
-            )}
+          <QuestionList
+            questions={editorItems}
+            showSearch={false}
+            showSort={false}
+            showTotalCount={false}
           />
         </Collapse.Panel>
       </Collapse>
@@ -133,20 +91,15 @@ const itemProps = PropTypes.arrayOf(
 
 Dashboard.propTypes = {
   authorItems: itemProps,
-  authorTotalCountNumber: PropTypes.number,
   editorItems: itemProps,
   reviewerItems: itemProps,
-  withTotalCount: PropTypes.bool,
-
   onClickCreateQuestion: PropTypes.func.isRequired,
 }
 
 Dashboard.defaultProps = {
   authorItems: [],
-  authorTotalCountNumber: 0,
   editorItems: [],
   reviewerItems: [],
-  withTotalCount: false,
 }
 
 export default Dashboard
