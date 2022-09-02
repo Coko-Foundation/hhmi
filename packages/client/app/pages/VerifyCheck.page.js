45ab9f098e1378d6426ef57dffcf080bce3d51c9
Yannis Barlas <yannisbarlas@gmail.com>
2022-09-02 16:19:23 +0300

    fix(client): check if identity is verified in ensure verified login page

--- packages/client/app/pages/VerifyCheck.page.js
+++ packages/client/app/pages/VerifyCheck.page.js
@@ -1,24 +1,39 @@
 import React from 'react'
-import { useMutation } from '@apollo/client'
+import { Redirect } from 'react-router-dom'
+import { useQuery, useMutation } from '@apollo/client'
 
 import { VerifyCheck } from 'ui'
 
-import { RESEND_VERIFICATION_EMAIL_AFTER_LOGIN } from '../graphql'
+import { CURRENT_USER, RESEND_VERIFICATION_EMAIL_AFTER_LOGIN } from '../graphql'
 
 const VeriryCheckPage = props => {
   const [verifyingLoader, setVerifyingLoader] = React.useState(false)
   const loaderDelay = 2000
 
+  const {
+    data: currentUserData,
+    // loading: currentUserLoading,
+    error: currentUserError,
+  } = useQuery(CURRENT_USER)
+
   const [resendMutation, { data, loading, error }] = useMutation(
     RESEND_VERIFICATION_EMAIL_AFTER_LOGIN,
   )
 
-  if (error) console.error(error)
+  if (error || currentUserError) console.error(error)
 
   const resend = () => {
     resendMutation().catch(e => console.error(e))
     setVerifyingLoader(true)
     setTimeout(() => setVerifyingLoader(false), loaderDelay)
+  }
+
+  const currentUser = currentUserData?.currentUser
+
+  if (!currentUser) return null
+
+  if (currentUser?.defaultIdentity.isVerified) {
+    return <Redirect to="/" />
   }
 
   return (
