b89a5cc662013cfdfcb574f52c20c65599615add
Grigor Malo <grigormalo@gmail.com>
2023-03-21 10:01:18 +0000

    refactor Question page, update page heading, fix loading conditions and indicator

--- packages/client/app/ui/question/Question.js
+++ packages/client/app/ui/question/Question.js
@@ -863,6 +863,7 @@ const Question = props => {
           <Button
             onClick={() => {
               confirmNewVersion.destroy()
+              // TODO: add error handling for this action
               onCreateNewVersion()
             }}
             status="danger"
@@ -1314,8 +1315,6 @@ const Question = props => {
     [agreedTc],
   )
 
-  if (loading || !metadata || !resources?.length) return <Spin spinning />
-
   return (
     <ModalContext.Provider value={contextValue}>
       <Wrapper>
@@ -1405,12 +1404,12 @@ Question.propTypes = {
 
   editorContent: PropTypes.shape(),
   questionAgreedTc: PropTypes.bool,
-  submitting: PropTypes.bool.isRequired,
-  isPublished: PropTypes.bool.isRequired,
-  isRejected: PropTypes.bool.isRequired,
-  isSubmitted: PropTypes.bool.isRequired,
-  isUnderReview: PropTypes.bool.isRequired,
-  isInProduction: PropTypes.bool.isRequired,
+  submitting: PropTypes.bool,
+  isPublished: PropTypes.bool,
+  isRejected: PropTypes.bool,
+  isSubmitted: PropTypes.bool,
+  isUnderReview: PropTypes.bool,
+  isInProduction: PropTypes.bool,
   isUserLoggedIn: PropTypes.bool,
   editorView: PropTypes.bool,
   showAssignHEButton: PropTypes.bool,
@@ -1677,6 +1676,12 @@ Question.defaultProps = {
   onEditorContentAutoSave: () => {},
   onImageUpload: () => {},
   onMetadataAutoSave: () => {},
+  submitting: false,
+  isPublished: false,
+  isRejected: false,
+  isSubmitted: false,
+  isUnderReview: false,
+  isInProduction: false,
   editorView: false,
   questionAgreedTc: false,
   showAssignHEButton: true,
