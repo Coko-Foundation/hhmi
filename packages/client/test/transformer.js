f1ef73b68da2df15a2fcccf230aeaf61bf9ced1b
Grigor Malo <grigormalo@gmail.com>
2023-07-05 08:23:00 +0000

    wax-prosemirror to 0.8.6

--- packages/client/test/transformer.js
+++ packages/client/test/transformer.js
@@ -3,4 +3,13 @@ const babelJest = require('babel-jest')
 
 module.exports = babelJest.createTransformer({
   presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+  env: {
+    testing: {
+      presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+    },
+  },
 })
