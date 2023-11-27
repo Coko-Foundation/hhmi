94fe34ab29c8a74a7ab626ff029cce59049a29de
Vukile Langa <vukilelanga@me.com>
2023-11-27 23:32:53 +0200

    feat(*): accept or reject reviewer invite

--- packages/server/services/notify/index.js
+++ packages/server/services/notify/index.js
@@ -61,6 +61,12 @@ class CokoNotifier {
     'hhmi.revokeInvitation': {
       notifyBy: ['email'],
     },
+    'hhmi.rejectInvitation': {
+      notifyBy: ['email'],
+    },
+    'hhmi.acceptInvitation': {
+      notifyBy: ['email'],
+    },
     // ...
   }
 
