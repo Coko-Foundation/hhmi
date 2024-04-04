9b6e14e96aec6b87891ce93f579feec10ba667bb
grgml <grigormalo@gmail.com>
2024-04-04 15:46:10 +0200

    refactor(server): remove condition from mailer config, just add port

--- packages/server/config/mailer.js
+++ packages/server/config/mailer.js
@@ -5,12 +5,9 @@ module.exports = {
   transport: {
     host: MAILER_HOSTNAME,
     port: MAILER_PORT,
-    ...(MAILER_USER &&
-      MAILER_PASSWORD && {
-        auth: {
-          user: MAILER_USER,
-          pass: MAILER_PASSWORD,
-        },
-      }),
+    auth: {
+      user: MAILER_USER,
+      pass: MAILER_PASSWORD,
+    },
   },
 }
