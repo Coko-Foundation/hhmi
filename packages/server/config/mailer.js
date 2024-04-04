466066e0174ac665008b87fedda9177b3a2ec4da
GrigorM <GrigorM@users.noreply.github.com>
2024-04-04 13:36:08 +0200

    use PLATFORM_SMTP_HOST variable in mailer config

--- packages/server/config/mailer.js
+++ packages/server/config/mailer.js
@@ -1,10 +1,10 @@
-const { MAILER_HOSTNAME, MAILER_PORT, MAILER_USER, MAILER_PASSWORD } =
+const { PLATFORM_SMTP_HOST, MAILER_PORT, MAILER_USER, MAILER_PASSWORD } =
   process.env
 
 module.exports = {
   transport: {
-    host: MAILER_HOSTNAME,
-    port: MAILER_PORT || 25,
+    host: PLATFORM_SMTP_HOST,
+    port: 25,
     ...(MAILER_USER &&
       MAILER_PASSWORD && {
         auth: {
